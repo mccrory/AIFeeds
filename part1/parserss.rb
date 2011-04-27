1b2750d08c026055755103fd518696a56cb5f4f0
Jason Brownlee <jason.brownlee05@gmail.com>
2011-04-27 11:33:33 +1000

    Fixed blogger atom parsing and article permalink

--- part1/parserss.rb
+++ part1/parserss.rb
@@ -4,6 +4,73 @@ require 'open-uri'
 require 'timeout'
 require 'thread'
 
+
+# fix: Blogspot feeds (atom) now have wrong post permalink
+# Regarding: http://code.google.com/p/feed-normalizer/issues/detail?id=30
+# from https://github.com/aldavidson/simple-rss
+# more specifically: https://github.com/aldavidson/simple-rss/blob/master/lib/simple-rss.rb
+class SimpleRSS
+  def parse
+    raise SimpleRSSError, "Poorly formatted feed" unless m = %r{<(channel|feed).*?>.*?</(channel|feed)>}mi.match(@source)
+
+    # Feed's title and link
+    feed_content = m[1] if  m = %r{(.*?)<(rss:|atom:)?(item|entry).*?>.*?</(rss:|atom:)?(item|entry)>}mi.match(@source)
+
+    @@feed_tags.each do |tag|
+      m = %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(feed_content) ||
+          %r{<(rss:|atom:)?#{tag}(.*?)\/\s*>}mi.match(feed_content)  ||
+          %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(@source) ||
+          %r{<(rss:|atom:)?#{tag}(.*?)\/\s*>}mi.match(@source)
+
+      if m && (m[2] || m[3])
+        tag_cleaned = clean_tag(tag)
+        instance_variable_set("@#{ tag_cleaned }", clean_content(tag, m[2],m[3]))
+        self.class.send(:attr_reader, tag_cleaned)
+      end
+    end
+
+    # RSS items' title, link, and description
+    @source.scan( %r{<(rss:|atom:)?(item|entry)([\s][^>]*)?>(.*?)</(rss:|atom:)?(item|entry)>}mi ) do |match|
+      item = Hash.new
+      @@item_tags.each do |tag|
+        if tag.to_s.include?("+")
+          tag_data = tag.to_s.split("+")
+          tag = tag_data[0]
+          rel = tag_data[1]
+
+          m = %r{<(rss:|atom:)?#{tag}(.*?)rel=['"]#{rel}['"](.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(match[3]) ||
+              %r{<(rss:|atom:)?#{tag}(.*?)rel=['"]#{rel}['"](.*?)/\s*>}mi.match(match[3])
+          item[clean_tag("#{tag}+#{rel}")] = clean_content(tag, m[3], m[4]) if m && (m[3] || m[4])
+        else
+          m = %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(match[3]) ||
+              %r{<(rss:|atom:)?#{tag}(.*?)/\s*>}mi.match(match[3])
+          item[clean_tag(tag)] = clean_content(tag, m[2],m[3]) if m && (m[2] || m[3])
+        end
+      end
+
+      # Hack to fix blogspot atom feed links pointing to comments issue
+      # Looks like the code here is just taking the FIRST link tag and using
+      # the href from that. In Blogspot atom feeds, this tends to be the link
+      # to the comments - not what we want.
+      # The RFC (http://www.ietf.org/rfc/rfc4287.txt) states that
+      # 'atom:link elements MAY have a "rel" attribute that indicates the link
+      # relation type.  If the "rel" attribute is not present, the link
+      # element MUST be interpreted as if the link relation type is
+      # "alternate"'
+      # Therefore we can work backwards and infer that the 'alternate' link,
+      # if present, should be taken as the default.
+      if item[:'link+alternate']
+        item[:link] = item[:'link+alternate']
+      end
+
+      def item.method_missing(name, *args) self[name] end
+
+      @items << item
+    end
+  end
+end
+
+
 RSS_TIMEOUT=15 # socket timeout in seconds
 THREADS=8 # number of threads for IO workers
 
@@ -91,7 +158,6 @@ def get_article_time(article)
   date = article.date_published if date.nil?
   return date
 end
-
 
 if __FILE__ == $0
   # parse feed
